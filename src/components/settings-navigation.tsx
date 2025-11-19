748e311623140a95a38e324733c526785dea15e7
KANAlV <editicroyedics@gmail.com>
2025-11-19 20:14:59 +0800

    fix: rules-of-hooks compliance

--- src/components/settings-navigation.tsx
+++ src/components/settings-navigation.tsx
@@ -2,27 +2,26 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
+export default function Navigation() {
+  const pathname = usePathname();
 
-export default function Navigation(){
-    function isActive(text: string) {
-        const pathname = usePathname();
-        return pathname.startsWith("/settings/" + text)
-            ? "border-b-5 border-blue-500"
-            : "";
-    }
+  function isActive(text: string) {
+    return pathname.startsWith("/settings/" + text)
+      ? "border-b-5 border-blue-500"
+      : "";
+  }
 
-
-    return(
-        <div className="w-full pt-6 h-1/12 border-b-2 border-gray-500">
-            <div className="text-4xl font-bold">Settings</div>
-            <div className="flex">
-                <Link href={"/settings/account"}>
-                    <div className={`${isActive("account")} p-4 pb-2`}> Account</div>
-                </Link>
-                <Link href={"/settings/profile"}>
-                    <div className={`${isActive("profile")} p-4 pb-2`}> Profile</div>
-                </Link>
-            </div>
-        </div>
-    )
+  return (
+    <div className="w-full pt-6 h-1/12 border-b-2 border-gray-500">
+      <div className="text-4xl font-bold">Settings</div>
+      <div className="flex">
+        <Link href={"/settings/account"}>
+          <div className={`${isActive("account")} p-4 pb-2`}> Account</div>
+        </Link>
+        <Link href={"/settings/profile"}>
+          <div className={`${isActive("profile")} p-4 pb-2`}> Profile</div>
+        </Link>
+      </div>
+    </div>
+  );
 }
